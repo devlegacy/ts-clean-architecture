1e1bbc82e83beb7ed5de61b1d264f4e523bd56fc
Samuel R <srojas@ggtech.es>
2022-09-23 17:44:55 -0500

    wip: update packages and settings

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,4 @@
 //// <reference types="./types"/> TODO: Improve load of types
 
 import 'reflect-metadata'
-// import './src/apps/mooc/backend/dependency-injection/index'
+// import '@/apps/mooc/backend/dependency-injection'
