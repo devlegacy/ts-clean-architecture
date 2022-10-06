698b4230eead3549e2a934886981030affd4ed6c
Samuel R <srojas@ggtech.es>
2022-10-06 09:27:00 -0500

    chore(scaffold): improve settings files
    configure tsconfig, commitizen, linter, husky and vscode

---
+++ cz.js
@@ -0,0 +1,23 @@
+const engine = require('cz-conventional-changelog/engine');
+const types = require('conventional-commit-types');
+const configLoader = require('commitizen').configLoader;
+const config = configLoader.load() || {};
+
+const options = {
+  ...config,
+  maxLineWidth: config.maxHeaderWidth || 100,
+  maxHeaderWidth: config.maxLineWidth || 100
+}
+
+// Note: type + scope + short description = 100
+
+module.exports = engine({
+  ...options,
+  types: {
+    ...types.types,
+    wip: {
+      description: "A word in progress feature",
+      title: "Word in progress"
+    }
+  }
+});
