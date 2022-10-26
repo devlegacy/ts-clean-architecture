441359522338576baa59192f51c4f2a8864e7cd9
Samuel R <srojas@ggtech.es>
2022-10-26 07:15:06 -0500

    chore(main): improve env vars loader

--- src/apps/mooc/backend/main.ts
+++ src/apps/mooc/backend/main.ts
@@ -1,5 +1,8 @@
 import 'reflect-metadata'
 
+import dotenv from 'dotenv'
+import { expand } from 'dotenv-expand'
+
 import { fatalErrorHandler } from '@/Contexts/Shared/infrastructure/logger'
 
 import { MoocBackendApp } from './MoocBackendApp'
@@ -7,6 +10,9 @@ import { MoocBackendApp } from './MoocBackendApp'
 process.on('uncaughtException', fatalErrorHandler).on('unhandledRejection', fatalErrorHandler)
 
 try {
+  const config = dotenv.config()
+  expand(config)
+
   new MoocBackendApp().start().catch((e) => fatalErrorHandler(e as Error))
 } catch (e) {
   fatalErrorHandler(e as Error)
