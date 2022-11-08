6ef62420309c6236f2ae5e25c504d8a8d1f9995e
Samuel R <srojas@ggtech.es>
2022-11-08 18:54:36 -0500

    docs(course): add comments in repository

--- src/Contexts/Backoffice/Courses/infrastructure/persistence/MikroOrmMongoBackofficeCourseRepository.ts
+++ src/Contexts/Backoffice/Courses/infrastructure/persistence/MikroOrmMongoBackofficeCourseRepository.ts
@@ -10,14 +10,24 @@ export class MikroOrmMongoBackofficeCourseRepository
   extends MikroOrmMongoRepository<BackofficeCourse>
   implements BackofficeCourseRepository
 {
+  // constructor(
+  //   @inject(SHARED_TYPES.MongoClient) client: Promise<MikroORM<MongoDriver>>,
+  //   @inject(SHARED_TYPES.QueryBus) private readonly queryBus: QueryBus
+  // ) {
+  //   super(client)
+  // }
+
   async searchBy(_criteria: Criteria): Promise<BackofficeCourse[]> {
     return []
   }
+
   save(course: BackofficeCourse): Promise<void> {
     return this.persist(course)
   }
 
   async all(): Promise<BackofficeCourse[]> {
+    // const courses = await this.queryBus.ask<CoursesResponse>(new SearchAllCoursesQuery())
+
     const repository = await this.repository()
     const courses = await repository.find({})
 
