0b7d77192c957c535d22c10bbfef41260a0fc8b4
Samuel R <srojas@ggtech.es>
2023-01-11 02:36:17 -0500

    style(eslint): improve config

--- .eslintrc.js
+++ .eslintrc.js
@@ -70,7 +70,149 @@ module.exports = {
     yoda: 'error',
     // Plugins
     '@typescript-eslint/array-type': 'warn',
-    '@typescript-eslint/member-ordering': 'warn',
+    '@typescript-eslint/member-ordering': ['warn', {
+      "default": [
+        // Index signature
+        "signature",
+        "call-signature",
+
+        // Fields
+        "public-static-field",
+        "protected-static-field",
+        "private-static-field",
+        "#private-static-field",
+
+        "public-decorated-field",
+        "protected-decorated-field",
+        "private-decorated-field",
+
+        "public-instance-field",
+        "protected-instance-field",
+        "private-instance-field",
+        "#private-instance-field",
+
+        "public-abstract-field",
+        "protected-abstract-field",
+
+        "public-field",
+        "protected-field",
+        "private-field",
+        "#private-field",
+
+        "static-field",
+        "instance-field",
+        "abstract-field",
+
+        "decorated-field",
+
+        "field",
+
+        // Static initialization
+        "static-initialization",
+
+        "public-instance-get",
+
+        // Constructors
+        "public-constructor",
+        "protected-constructor",
+        "private-constructor",
+
+        "constructor",
+
+        // Getters
+        "public-static-get",
+        "protected-static-get",
+        "private-static-get",
+        "#private-static-get",
+
+        "public-decorated-get",
+        "protected-decorated-get",
+        "private-decorated-get",
+
+        // "public-instance-get",
+        "protected-instance-get",
+        "private-instance-get",
+        "#private-instance-get",
+
+        "public-abstract-get",
+        "protected-abstract-get",
+
+        "public-get",
+        "protected-get",
+        "private-get",
+        "#private-get",
+
+        "static-get",
+        "instance-get",
+        "abstract-get",
+
+        "decorated-get",
+
+        "get",
+
+        // Setters
+        "public-static-set",
+        "protected-static-set",
+        "private-static-set",
+        "#private-static-set",
+
+        "public-decorated-set",
+        "protected-decorated-set",
+        "private-decorated-set",
+
+        "public-instance-set",
+        "protected-instance-set",
+        "private-instance-set",
+        "#private-instance-set",
+
+        "public-abstract-set",
+        "protected-abstract-set",
+
+        "public-set",
+        "protected-set",
+        "private-set",
+        "#private-set",
+
+        "static-set",
+        "instance-set",
+        "abstract-set",
+
+        "decorated-set",
+
+        "set",
+
+        // Methods
+        "public-static-method",
+        "protected-static-method",
+        "private-static-method",
+        "#private-static-method",
+
+        "public-decorated-method",
+        "protected-decorated-method",
+        "private-decorated-method",
+
+        "public-instance-method",
+        "protected-instance-method",
+        "private-instance-method",
+        "#private-instance-method",
+
+        "public-abstract-method",
+        "protected-abstract-method",
+
+        "public-method",
+        "protected-method",
+        "private-method",
+        "#private-method",
+
+        "static-method",
+        "instance-method",
+        "abstract-method",
+
+        "decorated-method",
+
+        "method"
+      ]
+    }],
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unnecessary-type-assertion': 'error',
     '@typescript-eslint/no-unused-vars': [
@@ -105,7 +247,7 @@ module.exports = {
       plugins: ['jest'],
       extends: ['plugin:jest/recommended', 'plugin:jest/style'],
       rules: {
-        'max-lines-per-function':'off',
+        'max-lines-per-function': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         'jest/prefer-expect-assertions': 'off',
         'jest/no-standalone-expect': 'off',
