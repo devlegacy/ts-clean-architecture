13c6eedd90c415b6d12671587870941f49b3bcf2
Samuel R <srojas@ggtech.es>
2023-05-07 04:49:28 -0500

    imp(types): add scaffold

---
+++ types/infrastructure/http.d.ts
@@ -0,0 +1,16 @@
+// index.d.
+
+// Type {Request} presenta problemas con superTest y express
+
+// Temp typing - only for dev purposes
+// type Request<
+//   RouteGeneric extends import('fastify').RouteGenericInterface = {
+//     Body: Record<string, any>
+//     Headers: Record<string, any>
+//     Params: Record<string, any>
+//     Querystring: Record<string, any>
+//   }
+// > = import('fastify').FastifyRequest<RouteGeneric>
+
+// // Temp typing - only for dev purposes
+// type Response = import('fastify').FastifyReply
