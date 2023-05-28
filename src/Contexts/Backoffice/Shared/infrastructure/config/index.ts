ed30073df172221af52a75092e51a78aa8bb0dba
Samuel R <srojas@ggtech.es>
2023-05-28 02:31:53 -0500

    wip(config): play with config

--- src/Contexts/Backoffice/Shared/infrastructure/config/index.ts
+++ src/Contexts/Backoffice/Shared/infrastructure/config/index.ts
@@ -1,176 +1,187 @@
 import convict from 'convict'
+import convict_format_with_validator from 'convict-format-with-validator'
 import dotenv from 'dotenv'
 import { expand } from 'dotenv-expand'
 import { existsSync } from 'fs'
 import { resolve } from 'path'
 
-const config = dotenv.config()
-expand(config)
-const backOfficeConfig = convict({
-  app: {
-    host: {
-      doc: 'The IP address to bind.',
-      // format: 'ipaddress',
-      default: '0.0.0.0',
-      env: 'APP_HOST',
-    },
-    port: {
-      doc: 'The port to bind.',
-      format: 'port',
-      default: 8086,
-      env: 'APP_PORT',
-      arg: 'port',
-    },
-    debug: {
-      doc: 'The application debug mode.',
-      format: [true, false],
-      default: true,
-      env: 'APP_DEBUG',
-    },
-    name: {
-      doc: 'The application name.',
-      format: String,
-      default: 'TS clean architecture',
-      env: 'APP_NAME',
-    },
-    env: {
-      doc: 'The application environment.',
-      format: ['production', 'development', 'staging', 'test'],
-      default: 'development',
-      env: 'APP_ENV',
-    },
-  },
-  log: {
-    level: {
-      doc: 'The application log level.',
-      format: ['fatal', 'error', 'warn', 'info', 'debug', 'trace'],
-      default: 'info',
-      env: 'LOG_LEVEL',
-    } as const,
-  },
-  mongo: {
-    url: {
-      doc: 'The MongoDB connection URL.',
-      format: String,
-      env: 'MONGO_URL',
-      default: 'mongodb://127.0.0.1:27017/backoffice',
-    },
-  },
-  sentry: {
-    dsn: {
-      doc: 'The Sentry connection DSN.',
-      format: String,
-      env: 'SENTRY_DSN',
-      default: '',
-    },
-  },
-  rabbitmq: {
-    connectionSettings: {
-      username: {
-        doc: 'RabbitMQ username',
-        format: String,
-        env: 'RABBITMQ_USERNAME',
-        default: 'guest',
+const envConfig = dotenv.config()
+expand(envConfig)
+
+convict.addFormats(convict_format_with_validator)
+const backOfficeConfig = convict(
+  {
+    app: {
+      host: {
+        doc: 'The IP address to bind.',
+        format: 'ipaddress',
+        default: '0.0.0.0',
+        env: 'APP_HOST',
       },
-      password: {
-        doc: 'RabbitMQ password',
-        format: String,
-        env: 'RABBITMQ_PASSWORD',
-        default: 'guest',
+      port: {
+        doc: 'The port to bind.',
+        format: 'port',
+        default: 8086,
+        env: 'APP_PORT',
+        arg: 'port',
       },
-      vhost: {
-        doc: 'RabbitMQ virtual host',
-        format: String,
-        env: 'RABBITMQ_VHOST',
-        default: '/',
+      debug: {
+        doc: 'The application debug mode.',
+        format: [true, false],
+        default: true,
+        env: 'APP_DEBUG',
       },
-      connection: {
-        secure: {
-          doc: 'RabbitMQ secure protocol',
-          format: Boolean,
-          env: 'RABBITMQ_SECURE',
-          default: false,
-        },
-        hostname: {
-          doc: 'RabbitMQ hostname',
-          format: String,
-          env: 'RABBITMQ_HOSTNAME',
-          default: 'localhost',
-        },
-        port: {
-          doc: 'RabbitMQ amqp port',
-          format: Number,
-          env: 'RABBITMQ_PORT',
-          default: 5672,
-        },
-      },
-    },
-    exchangeSettings: {
       name: {
-        doc: 'RabbitMQ exchange name',
+        doc: 'The application name.',
         format: String,
-        env: 'RABBITMQ_EXCHANGE_NAME',
-        default: 'domain_events',
+        default: 'TS clean architecture',
+        env: 'APP_NAME',
+      },
+      env: {
+        doc: 'The application environment.',
+        format: ['production', 'development', 'staging', 'test'],
+        default: 'development',
+        env: 'APP_ENV',
       },
     },
-    maxRetries: {
-      doc: 'Max number of retries for each message',
-      format: Number,
-      env: 'RABBITMQ_MAX_RETRIES',
-      default: 3,
+    log: {
+      level: {
+        doc: 'The application log level.',
+        format: ['fatal', 'error', 'warn', 'info', 'debug', 'trace'],
+        default: 'info',
+        env: 'LOG_LEVEL',
+      } as const,
     },
-    retryTtl: {
-      doc: 'Ttl for messages in the retry queue',
-      format: Number,
-      env: 'RABBITMQ_RETRY_TTL',
-      default: 1000,
+    mongo: {
+      url: {
+        doc: 'The MongoDB connection URL.',
+        format: String,
+        env: 'MONGO_URL',
+        default: 'mongodb://127.0.0.1:27017/backoffice',
+      },
     },
-  },
-  elastic: {
-    url: {
-      doc: 'The Elastic connection URL',
-      format: String,
-      env: 'ELASTIC_URL',
-      default: 'http://localhost:9200',
+    sentry: {
+      dsn: {
+        doc: 'The Sentry connection DSN.',
+        format: String,
+        env: 'SENTRY_DSN',
+        default: '',
+      },
     },
-    indexName: {
-      doc: 'The Elastic index name for this context',
-      format: String,
-      env: 'ELASTIC_INDEX_NAME',
-      default: 'backofficecourses',
-    },
-    config: {
-      doc: 'The Elastic config for this context',
-      format: '*',
-      env: 'ELASTIC_CONFIG',
-      default: {
-        settings: {
-          index: {
-            number_of_replicas: 0, // for local development
+    rabbitmq: {
+      connectionSettings: {
+        username: {
+          doc: 'RabbitMQ username',
+          format: String,
+          env: 'RABBITMQ_USERNAME',
+          default: 'guest',
+        },
+        password: {
+          doc: 'RabbitMQ password',
+          format: String,
+          env: 'RABBITMQ_PASSWORD',
+          default: 'guest',
+        },
+        vhost: {
+          doc: 'RabbitMQ virtual host',
+          format: String,
+          env: 'RABBITMQ_VHOST',
+          default: '/',
+        },
+        connection: {
+          secure: {
+            doc: 'RabbitMQ secure protocol',
+            format: Boolean,
+            env: 'RABBITMQ_SECURE',
+            default: false,
+          },
+          hostname: {
+            doc: 'RabbitMQ hostname',
+            format: String,
+            env: 'RABBITMQ_HOSTNAME',
+            default: 'localhost',
+          },
+          port: {
+            doc: 'RabbitMQ amqp port',
+            format: Number,
+            env: 'RABBITMQ_PORT',
+            default: 5672,
           },
         },
-        mappings: {
-          properties: {
-            id: {
-              type: 'keyword',
-              index: true,
+      },
+      exchangeSettings: {
+        name: {
+          doc: 'RabbitMQ exchange name',
+          format: String,
+          env: 'RABBITMQ_EXCHANGE_NAME',
+          default: 'domain_events',
+        },
+      },
+      maxRetries: {
+        doc: 'Max number of retries for each message',
+        format: Number,
+        env: 'RABBITMQ_MAX_RETRIES',
+        default: 3,
+      },
+      retryTtl: {
+        doc: 'Ttl for messages in the retry queue',
+        format: Number,
+        env: 'RABBITMQ_RETRY_TTL',
+        default: 1000,
+      },
+    },
+    elastic: {
+      url: {
+        doc: 'The Elastic connection URL',
+        format: String,
+        env: 'ELASTIC_URL',
+        default: 'http://localhost:9200',
+      },
+      indexName: {
+        doc: 'The Elastic index name for this context',
+        format: String,
+        env: 'ELASTIC_INDEX_NAME',
+        default: 'backofficecourses',
+      },
+      config: {
+        doc: 'The Elastic config for this context',
+        format: '*',
+        env: 'ELASTIC_CONFIG',
+        default: {
+          settings: {
+            index: {
+              number_of_replicas: 0, // for local development
             },
-            name: {
-              type: 'text',
-              index: true,
-              fielddata: true,
-            },
-            duration: {
-              type: 'text',
-              index: true,
-              fielddata: true,
+          },
+          mappings: {
+            properties: {
+              id: {
+                type: 'keyword',
+                index: true,
+              },
+              name: {
+                type: 'text',
+                index: true,
+                fielddata: true,
+              },
+              duration: {
+                type: 'text',
+                index: true,
+                fielddata: true,
+              },
             },
           },
         },
       },
     },
   },
-})
+  {
+    env: {
+      APP_PORT: '8086',
+      APP_ENV: process.env.APP_ENV || 'development',
+    },
+  }
+)
 
 const filePaths = [
   resolve(`${__dirname}/default.json`),
