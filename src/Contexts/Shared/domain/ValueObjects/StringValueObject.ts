5a21687e3a4089cb1a191df5262c49c7309b2bd9
Samuel R <srojas@ggtech.es>
2023-07-17 00:49:18 -0500

    feat(vo): add purify to string value objects

--- src/Contexts/Shared/domain/ValueObjects/StringValueObject.ts
+++ src/Contexts/Shared/domain/ValueObjects/StringValueObject.ts
@@ -1,11 +1,16 @@
+import DOMPurify from 'dompurify'
+import { JSDOM } from 'jsdom'
+
 import { ValueObject } from './ValueObject'
 
+const { window } = new JSDOM('')
+const purify = DOMPurify(window)
 export abstract class StringValueObject extends ValueObject<string> {
   // readonly value: string
 
   constructor(value: string) {
     // this.value = value
-    super(value?.trim())
+    super(value ? purify.sanitize(value.trim()) : value)
   }
 
   // toString() {
