374b5440776fd6f9b9e87a6ce441e5d888621899
Samuel R. <samuel_-_rojas@hotmail.com>
2024-07-09 05:41:33 -0500

    chore(types): update FlatEnum type definition
    update the FlatEnum type definition to use the eslint-disable-next-line unused-imports/no-unused-vars rule instead of @typescript-eslint/no-unused-vars.

--- types/domain/FlatEnum.d.ts
+++ types/domain/FlatEnum.d.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line unused-imports/no-unused-vars
 type FlatEnum<T> = `${(typeof T)[keyof typeof T]}` | keyof typeof T | (string & NonNullable<unknown>)
