e3c63be04e2b886769e3c36464368f81e9a1d043
Samuel R. <samuel_-_rojas@hotmail.com>
2024-12-12 18:27:05 -0500

    chore(scaffold): add custom configuration for commit message linting

---
+++ commitlint.config.cjs
@@ -0,0 +1,46 @@
+const { default: config } = require('@commitlint/config-conventional')
+const { defaultConfig, RuleConfigSeverity } = require('cz-git')
+
+const typeEnums = config?.rules?.['type-enum']?.at(2) || []
+// import {
+//   defaultConfig,
+//   RuleConfigSeverity,
+// } from 'cz-git'
+// const RuleConfigSeverity = {
+//   Disabled: 0,
+//   Warning: 1,
+//   Error: 2,
+// }
+
+/**
+ * @type {import('cz-git').UserConfig}
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [RuleConfigSeverity.Error, 'always', 200],
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        ...typeEnums,
+        // 'imp', // REVIEW: Implement
+        // 'update', // REVIEW: Implement
+        'wip',
+      ],
+    ],
+  },
+  prompt: {
+    useEmoji: true,
+    useAI: false,
+    aiNumber: 5,
+    types: [
+      ...defaultConfig.types,
+      {
+        value: 'wip',
+        name: 'wip:      A work in progress feature',
+        emoji: ':construction:',
+      },
+    ],
+  },
+}
